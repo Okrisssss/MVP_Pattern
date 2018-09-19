f859a8d6a1119aa73a1cf6e485a41fbe00d61155
iachimov petru <p.iachimov199302@gmail.com>
2018-09-19 18:58:52 +0300

    first commit

---
+++ app/src/main/java/apple/example/com/mvp_design_pattern/MainActivity.java
@@ -0,0 +1,13 @@
+package apple.example.com.mvp_design_pattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
